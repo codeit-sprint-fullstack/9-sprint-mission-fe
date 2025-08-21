ff7a143baf89c955300de1c0af9274acc0095509
bean-baek <suhyunl1989@gmail.com>
2025-08-21 15:55:08 +0900

    [feat] input focus in / out 기능, 유효성 검사 추가

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-const el = document.querySelectorAll('form > input');
-const btn = document.querySelector('form > button');
+const el = document.querySelectorAll('.form_input');
+const btn = document.querySelector('#form_button');
 
-
-
-///////// 버튼 포커싱 동작 기능 /////////////////
-
-el.forEach(input => {
-    input.addEventListener('focusin', (e) => {
-        e.target.classList.add('focus');
+el.forEach((e) => {
+    e.addEventListener('focusin', () => {
+        e.classList.add('focus');
     });
-});
+    e.addEventListener('input', () => {
+        const msg = e.parentElement.querySelector('.error_msg');
+        const value = e.value.trim();
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-el.forEach(input => {
-    input.addEventListener('focusout', (e) => {
-        e.target.classList.remove('focus');
+    if(e.name === '이메일' && !emailPattern.test(value)) {
+            e.classList.add('invalid');
+            msg.textContent = `잘못된 이메일 형식입니다.`;
+        }
+    else if (e.name === '비밀번호' && e.value.trim().length < 8) {
+            e.classList.add('invalid');
+            msg.textContent = `비밀번호는 8자리 이상이어야 합니다.`;
+        }
+    else if (e.name === '비밀번호 확인' && e.value.trim() !== document.querySelector('input[name="비밀번호"]').value.trim()) {
+            e.classList.add('invalid');
+            msg.textContent = `비밀번호가 일치하지 않습니다.`;
+        }
+    else {
+        e.classList.remove('invalid');
+        msg.textContent = '';
+    };
     });
+    e.addEventListener('focusout', () => {
+        e.classList.remove('focus');
+        if (e.value.trim() === '') {
+            e.classList.add('invalid');
+            const msg = e.parentElement.querySelector('.error_msg');
+            msg.textContent = `${e.placeholder}`;
+        } else {
+            e.classList.remove('invalid');
+            const msg = e.parentElement.querySelector('.error_msg');
+            msg.textContent = '';
+        }
+    });
+
 });
 
