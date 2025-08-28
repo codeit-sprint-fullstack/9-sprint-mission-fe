d366c9a851102c99d52a7d599f2b377ed5eab8ef
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-08-28 14:10:45 +0900

    create password validation

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ const emailValidation = function(value, box, warning) {
     if(!pattern.test(value)){
       box.classList.add("input-warning");
       warning.classList.add("message-on");
-      warning.textContent = "잘못된 이메일 형식입니다";
+      warning.textContent = "잘못된 이메일 형식입니다.";
       return false;
     }else{
       box.classList.remove("input-warning");
@@ -18,27 +18,42 @@ const emailValidation = function(value, box, warning) {
   }else{
     box.classList.add("input-warning");
     warning.classList.add("message-on");
-    warning.textContent = "이메일을 입력해주세요";
+    warning.textContent = "이메일을 입력해주세요.";
     return false;
   }
 }
 
 const passwordValidation = function(value, box, warning) {
-  
+  console.log(value.length)
+  if(!value){
+    box.classList.add("input-warning");
+    warning.classList.add("message-on");
+    warning.textContent = "비밀번호를 입력해주세요.";
+    return false;
+  }else if(value.length < 8){
+      box.classList.add("input-warning");
+      warning.classList.add("message-on");
+      warning.textContent = "비밀번호를 8자 이상 입력해주세요.";
+      return false;
+  }else{
+      box.classList.remove("input-warning");
+      warning.classList.remove("message-on");
+      warning.textContent = "";
+      return true;
+  }
 }
 
 const loginFormValidation = function(event) {
   const inputTarget = event.target; 
   const inputOutbox = inputTarget.parentElement;
   const inputValue = inputTarget.value;
-  const inputName = inputTarget.name;
   const inputType = inputTarget.type;
   const warningMessage = inputOutbox.parentElement.querySelector(".warning-message");
 
   if(inputType === "email"){
     emailValidation(inputValue, inputOutbox, warningMessage);
   }else if(inputType === "password"){
-    passwordValidation(inputValue, inputOutbox, warningMessage)
+    passwordValidation(inputValue, inputOutbox, warningMessage);
   }else{
     return
   }
