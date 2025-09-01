56f02d51d59554697ab76ad908a9994630d63af1
bean-baek <suhyunl1989@gmail.com>
2025-09-01 18:50:53 +0900

    [feat] 로그인, 회원가입 반응형 추가 및 모달 배경 추가

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const USER_DATA = [
 
 function userAlert(msg){
     const modalStatus = document.querySelector('.modal_container');
+    const moodalBg = document.querySelector('.modal_overlay')
     const alertMsg = document.querySelector('.modal_text');
     const alertBtn = document.querySelector('.modal_btn');
 
@@ -21,6 +22,7 @@ function userAlert(msg){
 
     alertMsg.textContent = msg;
     modalStatus.style.display = "flex";
+    moodalBg.style.display = "unset"
     alertBtn.addEventListener('click', () => (modalStatus.style.display = "none"));
 };
 
