1a7b6815a4c89774281c982dd6f646a29749f674
jieun318 <jieun031800@naver.com>
2025-09-05 17:47:49 +0900

    feat:article (목록,게시글,생성, 수정,삭제)구현

--- assets/script/ArticleService.js
+++ assets/script/ArticleService.js
@@ -17,5 +17,35 @@ const request=(url,options={})=>{
 /*개시글 목록 가져오기*/
 export const getArticleList = (page = 1, pageSize = 10, keyword = '') => {
   const queryParams = new URLSearchParams({ page, pageSize, keyword });
-  return request(`${API_BASE_URL}/article?${queryParams}`);
+  return request(`${API_BASE_URL}/articles?${queryParams}`);
+};
+
+/*게시글 가져오기*/ 
+export const getArticle=(id)=>{
+    return request(`${API_BASE_URL}/articles?${id}`);
+}
+
+/*게시글 생성 */
+export const createArticle = (articleData) => {
+  return request(`${API_BASE_URL}/articles`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(articleData),
+  });
+};
+
+/*게시글 수정*/ 
+export const patchArticle = (id, articleData) => {
+  return request(`${API_BASE_URL}/articles/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(articleData),
+  });
+};
+/*게시글 삭제*/ 
+export const deleteArticle=(id)=>{
+    return request(`{API_BASE_URL}/article/{id}`,{
+        method :'DELETE',
+
+    });
 };
