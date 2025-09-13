74a7381654785c3c768d3d13b0f3b4b4559521e2
rklpoi5678 <rklpoi5678@naver.com>
2025-09-13 18:40:27 +0900

    Design: 기본적인 디자인수정 ,module.css 로 변경

--- src/components/Card/CardList.jsx
+++ src/components/Card/CardList.jsx
@@ -1,20 +1,21 @@
 import Card from "./Card";
+import styles from './CardList.module.css'
 
 const dummyData =[
-  { title: 'item1', price: '500,000'},
-  { title: 'item1', price: '500,000'},
-  { title: 'item1', price: '500,000'},
-  { title: 'item1', price: '500,000'},
-  { title: 'item1', price: '500,000'},
+  { title: '아이패드 미니 팝니다', price: '500,000'},
+  { title: '아이패드 미니 팝니다', price: '500,000'},
+  { title: '아이패드 미니 팝니다', price: '500,000'},
+  { title: '아이패드 미니 팝니다', price: '500,000'},
+  { title: '아이패드 미니 팝니다', price: '500,000'},
 ];
 
 function CardList() {
   return (
-    <>
+    <div className={styles.cardContainer}>
       {dummyData.map((item) => (
         <Card key={item} title={item.title} price={item.price}/>
       ))}
-    </>
+    </div>
   )
 }
 
