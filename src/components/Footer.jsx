d1e7ee64d93527b09cfd32789ee403eb880263b6
rklpoi5678 <rklpoi5678@naver.com>
2025-09-13 18:40:27 +0900

    Design: 기본적인 디자인수정 ,module.css 로 변경

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,19 @@
+import styles from './Footer.module.css'
 
 function Footer() {
   return (
-    <footer>
-      <div class="footer-div">
-        <p>@codeit - 2024</p>
-        <div class="footer-link">
-          <a href="./privacy">Privacy Policy</a>
-          <a href="./faq">FAQ</a>
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerDiv}>
+        <p className={styles.footerDivPara}>@codeit - 2024</p>
+        <div className={styles.footerLink}>
+          <a className={styles.footerDivLikes} href="./privacy">Privacy Policy</a>
+          <a className={styles.footerDivLikes} href="./faq">FAQ</a>
         </div>
-        <div class="sns-images">
-          <a href="https://www.youtube.com/results?search_query=코드잇_판다마켓" target="_blank"><img src="./public/images/sns/ic_youtube.svg" alt="youtube" /></a>
-          <a href="https://www.facebook.com/" target="_blank"><img src="./public/images/sns/ic_fb.svg" alt="facebook" /></a>
-          <a href="https://www.instagram.com/" target="_blank"><img src="./public/images/sns/ic_instagram.svg" alt="instagram" /></a>
-          <a href="https://www.twitter.com/" target="_blank"><img src="./public/images/sns/ic_twitter.svg" alt="twitter" /></a>
+        <div className={styles.snsImages}>
+          <a className={styles.footerDivLikes} href="https://www.youtube.com/results?search_query=코드잇_판다마켓" target="_blank"><img src="/images/sns/ic_youtube.svg" alt="youtube" /></a>
+          <a className={styles.footerDivLikes} href="https://www.facebook.com/" target="_blank"><img src="/images/sns/ic_fb.svg" alt="facebook" /></a>
+          <a className={styles.footerDivLikes} href="https://www.instagram.com/" target="_blank"><img src="/images/sns/ic_instagram.svg" alt="instagram" /></a>
+          <a className={styles.footerDivLikes} href="https://www.twitter.com/" target="_blank"><img src='/images/sns/ic_twitter.svg' alt="twitter" /></a>
         </div>
       </div>
     </footer>
--- src/pages/items/ItemPage.jsx
+++ src/pages/items/ItemPage.jsx
@@ -1,32 +1,43 @@
 import ItemHeader from "@/components/Nav/ItemHeader"
-import Button from "@/components/Button/Button"
+import DropDown from "@/components/Button/DropDown"
 import Footer from "@/components/Footer"
 import CardList from "@/components/Card/CardList"
+import styles from './itempage.module.css'
 
 export default function ItemPage() {
   return (
     <>
       <ItemHeader />
+      <main className={styles.mainContainer}>
 
-      <div>
-        <p>베스트 상품</p>
-        <CardList />
-      </div>
-
-      <div>
-        <div>
-          <p>판매 중인 상품</p>
-          <input />
-          <button>상품 등록하기</button>
-          <Button />
+        <div className={styles.bestItemList}>
+          <p className={styles.bestItemPara}>베스트 상품</p>
+          <CardList />
         </div>
-        <CardList />
-      </div>
+
+        <div>
+          <div className={styles.sellItemContainer}>
+            <p className={styles.sellItemPara}>판매 중인 상품</p>
+            <div className={styles.sellItemFilter}>
+              <input className={styles.sellItemInput} placeholder="검색할 상품을 입력해주세요" />
+              <button className={styles.sellItemButton} >상품 등록하기</button>
+              <DropDown />
+            </div>
+          </div>
+          <CardList />
+        </div>
+      </main>
 
       <div>
         {/** pagination */}
+        <ul className={styles.pagination}>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
       </div>
-
       <Footer />
     </>
   )
