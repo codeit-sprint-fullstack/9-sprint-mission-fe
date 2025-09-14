60cd233aa990d3d008d6dc1cce32fc76aeaf2332
rklpoi5678 <rklpoi5678@naver.com>
2025-09-14 22:17:14 +0900

    Feat: pagination기능 구현

--- src/components/UI/Card/CardList.jsx
+++ src/components/UI/Card/CardList.jsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
 import styles from './CardList.module.css'
-import { getProduct, getProductList } from '@/api/ProductService'
+import { getProductList } from '@/api/ProductService'
+import { Pagination } from "@/components/Pagination/Pagination";
 
-function CardList({ type }) {
+function CardList({ currentPage, itemsPerPage, setTotalItems, type }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     async function fetchProduct() {
       try {
         let data = [];
-        if(type === 'favorite') {
-          data = await getProductList(1,4,'','favorite');
+        if (type === 'favorite') {
+          data = await getProductList(1, 4, '', 'favorite');
         } else {
-          data = await getProduct(); // 전체상품
+          data = await getProductList(currentPage, itemsPerPage); // 전체상품
         }
         setProducts(data.list);
+        setTotalItems(data.totalCount);
       } catch (err) {
-        console.error('Failed getProcut:', err)
+        console.error('Failed getProduct:', err);
       }
     }
 
     fetchProduct()
-  }, [type])
+  }, [type, currentPage, itemsPerPage, setTotalItems]);
 
   return (
     <div className={type === 'favorite' ? styles.favoriteContainer : styles.cardContainer}>
