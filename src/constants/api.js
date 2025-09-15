d6d52c8faa713db0450669408cc3434134d5e174
rklpoi5678 <rklpoi5678@naver.com>
2025-09-15 16:05:48 +0900

    Rename: 일관된 엔드포인트로 수정

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 import axios from "axios";
 export const BASE_URL = "https://panda-market-api.vercel.app/";
 export const app = axios.create({
   baseURL: "https://panda-market-api.vercel.app",
 });
 
+=======
+export const BASE_URL = "https://panda-market-api.vercel.app/";
+>>>>>>> c5e97a6 (Rename: 일관된 엔드포인트로 수정)
