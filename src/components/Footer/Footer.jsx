81db1a13e21d52b946255da27db4785f7ef7ee20
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-15 13:48:00 +0900

    chore: modify image path

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
-import facebookIcon from '../../assets/img/ic_facebook.png';
-import twitterIcon from '../../assets/img/ic_twitter.png';
-import youtubeIcon from '../../assets/img/ic_youtube.png';
-import instagramIcon from '../../assets/img/ic_instagram.png';
+import facebookIcon from '@/assets/img/ic_facebook.png';
+import twitterIcon from '@/assets/img/ic_twitter.png';
+import youtubeIcon from '@/assets/img/ic_youtube.png';
+import instagramIcon from '@/assets/img/ic_instagram.png';
 
 export function Footer() {
   return (
