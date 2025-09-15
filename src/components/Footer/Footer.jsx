d431972e64b2dfa34dbedcc19fc1914073ea73c2
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-15 11:36:13 +0900

    feat: add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import facebookIcon from '../../assets/img/ic_facebook.png';
+import twitterIcon from '../../assets/img/ic_twitter.png';
+import youtubeIcon from '../../assets/img/ic_youtube.png';
+import instagramIcon from '../../assets/img/ic_instagram.png';
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <div id="footer-box">
+        <p id="copyright">©codeit - 2024</p>
+        <ul id="cs-list">
+          <li><a href="/privacy">Privacy Policy</a></li>
+          <li><a href="/faq">FAQ</a></li>
+        </ul>
+        <ul id="sns-list">
+          <li><a href="https://facebook.com" target='_blank'><img src={ facebookIcon } /></a></li>
+          <li><a href="https://x.com" target='_blank'><img src={ twitterIcon } /></a></li>
+          <li><a href="https://youtube.com" target='_blank'><img src={ youtubeIcon } /></a></li>
+          <li><a href="https://instagram.com" target='_blank'><img src={ instagramIcon } /></a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/style.css'
+import Item from './Item.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Item />
+    <Footer />
   </StrictMode>,
 )
