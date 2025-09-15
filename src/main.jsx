7d662a034951d96ac3c6c9b96cf91fcbe0007818
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-15 14:39:29 +0900

    fix: not imported css problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/style.css'
-import Item from './Item.jsx'
-import { Footer } from './components/Footer'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/styles/reset.css';
+import '@/styles/fontface.css';
+import '@/styles/style.css';
+import Item from '@/Item.jsx';
+//import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* <Header /> */}
     <Item />
     <Footer />
   </StrictMode>,
-)
+);
