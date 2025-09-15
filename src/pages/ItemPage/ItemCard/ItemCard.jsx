cc283e4c51387a663ce48acf46a83d0cc503a1ab
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-16 00:01:09 +0900

    feat: modularize css for item card

--- src/pages/ItemPage/ItemCard/ItemCard.jsx
+++ src/pages/ItemPage/ItemCard/ItemCard.jsx
@@ -1,16 +1,16 @@
-import './ItemCard.css';
+import style from './ItemCard.module.css';
 import likesIcon from '@/assets/img/ic_likes.svg';
 
 export function ItemCard() {
   return (
-    <div className="item-card best">
-      <img className="item-img" src="" alt="상품 이미지" />
-      <div className="item-text-wrap">
-        <h3 className="item-title">제목</h3>
-        <p className="item-price">30000원</p>
-        <p className="item-likes">
-          <img className="likes-icon" src={likesIcon} alt="좋아요" />
-          <span className="likes-count">320</span>
+    <div className={style.itemCard}>
+      <img className={style.itemImg} src="/" alt="상품 이미지" />
+      <div className={style.itemTextWrap}>
+        <h3 className={style.itemTitle}>제목</h3>
+        <p className={style.itemPrice}>30000원</p>
+        <p className={style.itemLikes}>
+          <img className={style.likesIcon} src={likesIcon} alt="좋아요" />
+          <span className={style.likesCount}>320</span>
         </p>
       </div>
     </div>
