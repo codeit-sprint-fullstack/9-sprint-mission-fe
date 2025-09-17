b4fccbaed22c476a0ed3845511896777e27e04e9
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-17 14:30:08 +0900

    feat: set best item list use context

--- src/pages/ItemPage/BestItemList/BestItemList.jsx
+++ src/pages/ItemPage/BestItemList/BestItemList.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import style from './BestItemList.module.css';
+import { useContext } from 'react';
+import styles from './BestItemList.module.css';
 import { ItemCard } from '@/pages/ItemPage/ItemCard';
+import { ItemContext } from '@/contexts/ItemContext.js';
 
 export function BestItemList() {
+  const {
+    best: { itemList: bestItemList, isLoading, error },
+  } = useContext(ItemContext);
+
+  if (isLoading) return <p>로딩 중...</p>;
+  if (error) return <p>에러가 발생했습니다.</p>;
+
   return (
-    <div className={style.itemLsit}>
-      <ItemCard isParentBest={true} />
+    <div className={styles.itemLsit}>
+      {bestItemList.map((item) => (
+        <ItemCard key={item.id} itemValue={item} isParentBest={true} />
+      ))}
     </div>
   );
 }
