da6eb2278a546c1f24aec00e5afd7bd83b15fd53
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-17 14:40:21 +0900

    feat: set porvider for sales list

--- src/providers/ItemProvider.jsx
+++ src/providers/ItemProvider.jsx
@@ -3,7 +3,7 @@ import { getProductList } from '../api/ProductService.js';
 import { ItemContext } from '@/contexts/ItemContext.js';
 
 const BEST_ITEM_PAGE_SIZE = 4;
-const SALES_ITEM_PAGE_SIZE = 8;
+const SALES_ITEM_PAGE_SIZE = 10;
 const ITEM_PAGE_NUM = 1;
 
 export const ItemProvider = ({ children }) => {
@@ -11,6 +11,13 @@ export const ItemProvider = ({ children }) => {
   const [bestItemList, setBestItemList] = useState([]);
   const [isBestLoading, setIsBestLoading] = useState(true);
   const [bestError, setBestError] = useState(null);
+
+  // State for Sales Items
+  const [salesItemList, setSalesItemList] = useState([]);
+  const [isSalesLoading, setIsSalesLoading] = useState(true);
+  const [salesError, setSalesError] = useState(null);
+  const [salesSearchTerm, setSalesSearchTerm] = useState('');
+  const [salesOrderBy, setSalesOrderBy] = useState('recent');
 
   useEffect(() => {
     const getBestItems = async () => {
@@ -33,11 +40,42 @@ export const ItemProvider = ({ children }) => {
     getBestItems();
   }, []);
 
+  useEffect(() => {
+    const getSalesItems = async () => {
+      setIsSalesLoading(true);
+      setSalesError(null);
+      try {
+        const params = {
+          page: ITEM_PAGE_NUM,
+          pageSize: SALES_ITEM_PAGE_SIZE,
+          orderBy: salesOrderBy,
+        };
+        if (salesSearchTerm) {
+          params.search = salesSearchTerm;
+        }
+        const data = await getProductList(params);
+        setSalesItemList(data.list);
+      } catch (err) {
+        setSalesError(err);
+      } finally {
+        setIsSalesLoading(false);
+      }
+    };
+    getSalesItems();
+  }, [salesSearchTerm, salesOrderBy]);
+
   const contextValue = {
     best: {
       itemList: bestItemList,
       isLoading: isBestLoading,
       error: bestError,
+    },
+    sales: {
+      itemList: salesItemList,
+      isLoading: isSalesLoading,
+      error: salesError,
+      setSearchTerm: setSalesSearchTerm,
+      setSalesOrderBy: setSalesOrderBy,
     },
   };
 
