8bb6e8fe1c92744b1a3f677657a03412e4b40a92
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-17 11:49:43 +0900

    fix: getProductList function can not throw error

--- src/api/ProductService.js
+++ src/api/ProductService.js
@@ -1,15 +1,11 @@
 const API_URL = "https://panda-market-api.vercel.app/products";
 
-export const getProductList = async function(page=1, pageSize=10, keyword="", orderBy="recent") {
+export const getProductList = async function(page, pageSize, keyword, orderBy) {
   const url = `${API_URL}?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`리퀘스트 에러: ${response.status}, 에러 메시지: ${response.statusText}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    return err.message;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`리퀘스트 에러: ${response.status}, 에러 메시지: ${response.statusText}`);
   }
+  const data = await response.json();
+  return data;
 }
