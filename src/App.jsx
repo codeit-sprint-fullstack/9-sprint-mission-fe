991dcbc631b2556f5cf2e6cac507a11895696831
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-18 09:41:07 +0900

    style: move importing header and footer componant main to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BestItemsSection } from './pages/ItemPage/BestItemsSection';
 import { SalesItemsSection } from './pages/ItemPage/SalesItemsSection';
 import { ItemProvider } from './providers/ItemProvider';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 function App() {
   return (
     <ItemProvider>
+      <Header />
       <main className="main item-main">
         <BestItemsSection />
         <SalesItemsSection />
       </main>
+      <Footer />
     </ItemProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,9 @@ import '@/styles/reset.css';
 import '@/styles/fontface.css';
 import '@/styles/style.css';
 import App from '@/App.jsx';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
     <App />
-    <Footer />
   </StrictMode>,
 );
