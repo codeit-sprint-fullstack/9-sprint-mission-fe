d18597569385e3fc7a1dc3aa7b31dc9cf45bb8ee
rklpoi5678 <rklpoi5678@naver.com>
2025-09-22 18:08:29 +0900

    Style: NavLink로 active시 3692FF색상변경, 호버시 navlink호비서 3692FF

--- src/components/UI/Nav/ItemHeader.jsx
+++ src/components/UI/Nav/ItemHeader.jsx
@@ -1,5 +1,5 @@
 import { useBreakPoint } from "@/hooks/useBreakpoint"
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import styles from './ItemHeader.module.css';
 
 export function ItemHeader() {
@@ -14,16 +14,26 @@ export function ItemHeader() {
           </div>
           <div className={styles.logoParaContainer}>
             <p className={styles.logoPara}>자유게시판</p>
-            <Link to="/products/items"><p className={styles.logoPara}>중고마켓</p></Link>
+            <NavLink to="/products/items"
+              className={({ isActive }) => isActive
+                ? `${styles.logoPara} ${styles.active}`
+                : styles.logoPara}
+            >
+              중고마켓
+            </NavLink>
           </div>
         </div>
-        {isDesktop && (
-          <Link className={styles.logoBoxLink} to="/login">로그인</Link>
-        )}
-        {(isTablet || isMobile) && (
-          <img className={styles.logoAvatar} src="/images/default_user_logo.svg" alt="판다마켓 로고" />
-        )}
-      </div>
-    </nav>
+        {
+          isDesktop && (
+            <Link className={styles.logoBoxLink} to="/login">로그인</Link>
+          )
+        }
+        {
+          (isTablet || isMobile) && (
+            <img className={styles.logoAvatar} src="/images/default_user_logo.svg" alt="판다마켓 로고" />
+          )
+        }
+      </div >
+    </nav >
   )
 }
