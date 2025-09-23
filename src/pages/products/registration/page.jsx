29259d8f735fe42c61aaba7b8fcf38ec0ab93798
rklpoi5678 <rklpoi5678@naver.com>
2025-09-23 22:13:19 +0900

    Feat: 폼 Post추가

--- src/pages/products/registration/page.jsx
+++ src/pages/products/registration/page.jsx
@@ -3,6 +3,8 @@ import { productDiscribeValidate, productNameValidate, productPriceValidate, pro
 import { X } from 'lucide-react';
 import styles from './RegistraionPage.module.css';
 import { useNavigate } from 'react-router-dom';
+import { LoginModal } from '@/components/Modal/LoginModal';
+import { instance } from '@/constants/api';
 
 export function RegistraionPage() {
   const [name, setName] = useState('');
@@ -15,7 +17,9 @@ export function RegistraionPage() {
   const [tagError, setTagError] = useState('');
   const [toggleBtn, setToggleBtn] = useState(true);
 
-  const navigate =  useNavigate();
+  const [showModal, setShowModal] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const isValid = name && discribe && price && tag &&
@@ -25,9 +29,9 @@ export function RegistraionPage() {
       setToggleBtn(true);
     } else {
       setToggleBtn(false);
-      navigate('/products/items');
     }
-  }, [nameError, discribeError, priceError, tagError]);
+    // navigate('/products/items');
+  }, [nameError, discribeError, priceError, tagError, name, discribe, price, tag, navigate]);
 
   const handleOnChageName = (e) => {
     setName(e.target.value.trim());
@@ -49,90 +53,133 @@ export function RegistraionPage() {
     setTagError(productTagValidate(tag));
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (toggleBtn) {
+      alert("입력 필드를 확인해주세요!");
+      return;
+    }
+
+    try {
+      const newItems = {
+        name,
+        description: discribe,
+        price: Number(price),
+        tags: tag,
+      };
+
+      const response = await instance.post('api/items', newItems, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status === 201) {
+        // 임시적인 리다이렉트
+        navigate('/products/items/detail/1');
+      }
+    } catch (error) {
+      // (기술부채x 미션중 개인 생각 있었으면 좋을것) toast 사용
+      console.error('등록중 오류 발생:', error);
+      setShowModal(true);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <form
-      className={styles.RegContainer}
-      method="POST"
-      action="/api/items"
-      autoComplete='off'
-    >
-      <div className={styles.RegTitle}>
-        <h2>상품 등록하기</h2>
-        <button className={toggleBtn ? styles.disActiveBtn : styles.activeBtn} type='submit' disabled={toggleBtn}>등록</button>
-      </div>
-      <div className={styles.RegProductName}>
-        <label htmlFor='product_name'>상품명</label>
-        <input
-          type="text"
-          name="product_name"
-          id="product_name"
-          onChange={handleOnChageName}
-          placeholder='상품명을 입력해주세요'
-          aria-label='상품명을 입력해주세요'
-          required
-        />
-        {nameError &&
-          <span className={styles.error}>{nameError}</span>
-        }
-      </div>
+    <>
+      <form
+        className={styles.RegContainer}
+        method="POST"
+        onSubmit={handleSubmit}
+        autoComplete='off'
+      >
+        <div className={styles.RegTitle}>
+          <h2>상품 등록하기</h2>
+          <button className={toggleBtn ? styles.disActiveBtn : styles.activeBtn} type='submit' disabled={toggleBtn}>등록</button>
+        </div>
+        <div className={styles.RegProductName}>
+          <label htmlFor='product_name'>상품명</label>
+          <input
+            type="text"
+            name="product_name"
+            id="product_name"
+            onChange={handleOnChageName}
+            placeholder='상품명을 입력해주세요'
+            aria-label='상품명을 입력해주세요'
+            required
+          />
+          {nameError &&
+            <span className={styles.error}>{nameError}</span>
+          }
+        </div>
 
-      <div className={styles.RegProductDescribe}>
-        <label htmlFor='product_describe'>상품 소개</label>
-        <textarea
-          name="product_describe"
-          id="product_describe"
-          onChange={handleOnChageDiscribe}
-          placeholder='상품 소개를 입력해주세요'
-          aria-label='상품 소개를 입력해주세요'
-          required
-        >
-        </textarea>
-        {discribeError &&
-          <span className={styles.error}>{discribeError}</span>
-        }
-      </div>
+        <div className={styles.RegProductDescribe}>
+          <label htmlFor='product_describe'>상품 소개</label>
+          <textarea
+            name="product_describe"
+            id="product_describe"
+            onChange={handleOnChageDiscribe}
+            placeholder='상품 소개를 입력해주세요'
+            aria-label='상품 소개를 입력해주세요'
+            required
+          >
+          </textarea>
+          {discribeError &&
+            <span className={styles.error}>{discribeError}</span>
+          }
+        </div>
 
-      <div className={styles.RegProductPrice}>
-        <label htmlFor='product_price'>판매가격</label>
-        <input
-          type="text"
-          name='product_price'
-          id='product_price'
-          onChange={handleOnChagePrice}
-          placeholder='판매 가격을 입력해주세요'
-          aria-label='판매 가격을 입력해주세요'
-          required
-        />
-        {priceError &&
-          <span className={styles.error}>{priceError}</span>
-        }
-      </div>
+        <div className={styles.RegProductPrice}>
+          <label htmlFor='product_price'>판매가격</label>
+          <input
+            type="text"
+            name='product_price'
+            id='product_price'
+            onChange={handleOnChagePrice}
+            placeholder='판매 가격을 입력해주세요'
+            aria-label='판매 가격을 입력해주세요'
+            required
+          />
+          {priceError &&
+            <span className={styles.error}>{priceError}</span>
+          }
+        </div>
 
-      <div className={styles.RegProductTag}>
-        <label htmlFor='product_tag'>태그</label>
-        <input
-          type="text"
-          name='product_tag'
-          id='product_tag'
-          onChange={handleOnChageTag}
-          placeholder='태그를 입력해주세요'
-          aria-label='태그를 입력해주세요'
-          required
-        />
-        {tagError &&
-          <span className={styles.error}>{tagError}</span>
-        }
-        <div className={styles.RegSelectTagsContainer}>
-          <div className={styles.RegSelectTags}>
-            <p>#티셔츠</p>
-            <X className={styles.TagIcons} width={18} height={18} color='white' />
-          </div>
-          <div className={styles.RegSelectTags}>
-            <p>#상의</p>
-            <X className={styles.TagIcons} width={18} height={18} color='white' />
+        <div className={styles.RegProductTag}>
+          <label htmlFor='product_tag'>태그</label>
+          <input
+            type="text"
+            name='product_tag'
+            id='product_tag'
+            onChange={handleOnChageTag}
+            placeholder='태그를 입력해주세요'
+            aria-label='태그를 입력해주세요'
+            required
+          />
+          {tagError &&
+            <span className={styles.error}>{tagError}</span>
+          }
+          <div className={styles.RegSelectTagsContainer}>
+            <div className={styles.RegSelectTags}>
+              <p>#티셔츠</p>
+              <X className={styles.TagIcons} width={18} height={18} color='white' />
+            </div>
+            <div className={styles.RegSelectTags}>
+              <p>#상의</p>
+              <X className={styles.TagIcons} width={18} height={18} color='white' />
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+
+      {showModal &&
+        <LoginModal close={handleCloseModal} msg={'등록중 예기치 못한 오류가 발생했습니다.\n 잠시후 다시시도해 주십시오 \n 문의(meta-os@zohomail.com)'} />
+      }
+    </>
   );
 }
