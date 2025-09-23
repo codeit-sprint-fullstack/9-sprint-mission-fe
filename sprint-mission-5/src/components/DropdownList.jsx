45b7ffc8433355f98592bcb9a17ef76a97adc38c
nnnn2001 <hee2ees2@gmail.com>
2025-09-23 15:22:42 +0900

    fix: 버튼 명칭 변경 및  css속성 변경

--- sprint-mission-5/src/components/DropdownList.jsx
+++ sprint-mission-5/src/components/DropdownList.jsx
@@ -8,7 +8,7 @@ function DropdownList({ onSortSelection }) {
         최신순
       </div>
       <div className={styles.dropdownItem} onClick={() => onSortSelection("favorite")}>
-        베스트순
+        좋아요순
       </div>
     </div>
   )
