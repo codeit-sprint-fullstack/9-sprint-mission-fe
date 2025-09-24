05ff659dd2eb97e96a2e7c15e7d585facac510ec
nnnn2001 <hee2ees2@gmail.com>
2025-09-24 10:51:35 +0900

    feat: TopBar/ Footer 반응형 디자인 적용

--- sprint-mission-5/src/components/TopBar.jsx
+++ sprint-mission-5/src/components/TopBar.jsx
@@ -6,16 +6,20 @@ function TopBar() {
   return (
     <header className={styles.topBar}>
       <div className={styles.topBarInner}>
-        <img src="../resources/img/logo.png" className={styles.logoIcon} alt="판다마켓 로고" />
-        <span className={styles.font}>판다마켓</span>
-        <a href="/login.html" className={styles.loginBtn} />
-        <ul className={styles.sideMenu}>
-          <li>자유게시판</li>
-          <li>중고마켓</li>
-        </ul>
-      </div>
+        <div className={styles.logoMenuContainer}>
+          <div className={styles.logoContainer}>
+            <img src="../resources/img/logo.png" className={styles.logoIcon} alt="판다마켓 로고" />
+            <span className={styles.pandaMarketFont}>판다마켓</span>
+          </div>
 
-      <button className={styles.login}>로그인</button>
+          <ul className={styles.sideMenu}>
+            <li>자유게시판</li>
+            <li>중고마켓</li>
+          </ul>
+        </div>
+
+        <button className={styles.login}>로그인</button>
+      </div>
     </header >
   );
 }
