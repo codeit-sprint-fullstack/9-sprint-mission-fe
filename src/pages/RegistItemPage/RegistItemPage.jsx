2070bfa3690f830022e885f9866869344134528f
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-24 14:45:04 +0900

    feat: import form vaildation code in item regist page

--- src/pages/RegistItemPage/RegistItemPage.jsx
+++ src/pages/RegistItemPage/RegistItemPage.jsx
@@ -1,7 +1,39 @@
 import clsx from 'clsx';
 import styles from './RegistItemPage.module.css';
+import { useInputValidation } from '@/hooks/useInputValidation';
+import * as validation from './validation.js';
 
 export function RegistItemPage() {
+  const warningMessage = (message = '') => {
+    return <p className="warning-message">{message}</p>;
+  };
+
+  const itemTitleInput = useInputValidation({
+    initialValue: '',
+    validate: validation.itemNameValidation,
+  });
+
+  const itemContextInput = useInputValidation({
+    initialValue: '',
+    validate: validation.itemContextValidation,
+  });
+
+  const itemPriceInput = useInputValidation({
+    initialValue: '',
+    validate: validation.itemPriceValidation,
+  });
+
+  const itemTagInput = useInputValidation({
+    initialValue: '',
+    validate: validation.itemTagValidation,
+  });
+
+  const isFromVaild =
+    itemTitleInput.isValid &&
+    itemContextInput.isValid &&
+    itemPriceInput.isValid &&
+    itemTagInput.isValid;
+
   return (
     <main className={clsx('main', styles.registMain)}>
       <form action="" className={styles.itemRegistForm}>
@@ -11,20 +43,30 @@ export function RegistItemPage() {
             type="submit"
             className="s-btn compact"
             value="등록"
-            disabled={true}
+            disabled={!isFromVaild}
           />
         </section>
         <section className={styles.formBody}>
           <div className={styles.inputSection}>
             <label htmlFor="item-name">상품명</label>
-            <div className="input-wrap large-form">
+            <div
+              className={clsx(
+                'input-wrap',
+                'large-form',
+                itemTitleInput.className,
+              )}
+            >
               <input
                 type="text"
                 id="item-name"
                 className="input-value"
+                value={itemTitleInput.value}
+                onChange={itemTitleInput.handleChange}
+                onBlur={itemTitleInput.handleBlur}
                 placeholder="상품명을 입력해주세요"
               />
             </div>
+            {!itemTitleInput.isValid ? warningMessage(itemTitleInput.err) : ''}
           </div>
           <div className={styles.inputSection}>
             <label htmlFor="item-context">상품 소개</label>
@@ -33,36 +75,59 @@ export function RegistItemPage() {
                 'input-wrap',
                 'large-form',
                 styles.inputTextareaWrap,
+                itemContextInput.className,
               )}
             >
               <textarea
                 id="item-context"
                 className={clsx('input-value', styles.inputTextarea)}
+                value={itemContextInput.value}
+                onChange={itemContextInput.handleChange}
+                onBlur={itemContextInput.handleBlur}
                 placeholder="상품 소개를 입력해주세요"
               />
             </div>
+            {!itemContextInput.isValid
+              ? warningMessage(itemContextInput.err)
+              : ''}
           </div>
           <div className={styles.inputSection}>
             <label htmlFor="item-price">판매 가격</label>
-            <div className="input-wrap large-form">
+            <div
+              className={clsx(
+                'input-wrap',
+                'large-form',
+                itemPriceInput.className,
+              )}
+            >
               <input
                 type="text"
                 id="item-price"
                 className="input-value"
+                value={itemPriceInput.value}
+                onChange={itemPriceInput.handleChange}
+                onBlur={itemPriceInput.handleBlur}
                 placeholder="판매 가격을 입력해주세요"
               />
             </div>
+            {!itemPriceInput.isValid ? warningMessage(itemPriceInput.err) : ''}
           </div>
           <div className={styles.inputSection}>
             <label htmlFor="item-tag">태그</label>
-            <div className="input-wrap large-form">
+            <div
+              className={clsx('input-wrap large-form', itemTagInput.className)}
+            >
               <input
                 type="text"
                 id="item-tag"
                 className="input-value"
+                value={itemTagInput.value}
+                onChange={itemTagInput.handleChange}
+                onBlur={itemTagInput.handleBlur}
                 placeholder="태그를 입력해주세요"
               />
             </div>
+            {!itemTagInput.isValid ? warningMessage(itemTagInput.err) : ''}
           </div>
         </section>
       </form>
