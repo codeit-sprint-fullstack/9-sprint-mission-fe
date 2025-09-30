36929d22dba7d16d0e474c3778969f1a82d4971e
rklpoi5678 <rklpoi5678@naver.com>
2025-10-01 00:03:39 +0900

    Design: 게시판 css수정, 댓글이 없을때 스타일수정

--- src/pages/articles/article-detail/page.jsx
+++ src/pages/articles/article-detail/page.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getArticleById } from "@/api/ArticleService";
-import { Heart } from "lucide-react";
+import { EllipsisVertical, EllipsisVerticalIcon, Heart, Undo2 } from "lucide-react";
 import styles from "./ArticleDetailPage.module.css";
 
 export function ArticleDetailPage() {
@@ -33,10 +33,21 @@ export function ArticleDetailPage() {
       {/* 게시글 제목 + 좋아요 */}
       <section className={styles.header}>
         <h2 className={styles.title}>{articles.title}</h2>
-        <button className={styles.likeBtn}>
-          <Heart className={styles.heartIcon} />
-          <span>123</span>
-        </button>
+        <div className={styles.authorBox}>
+          <div className={styles.authorMeta}>
+            <img src={articles.author?.userProfile?.photoUrl || "/images/logo.png"}
+              alt="authorAvatar"
+              className={styles.authorAvatar}
+            />
+            <p>{articles.author?.name}</p>
+            <span>{articles.author?.updatedAt}</span>
+          </div>
+          <div className={styles.authorBoxHr}></div>
+          <button className={styles.likeBtn}>
+            <Heart className={styles.heartIcon} />
+            <span>123</span>
+          </button>
+        </div>
       </section>
 
       {/* 본문 */}
@@ -48,7 +59,7 @@ export function ArticleDetailPage() {
       <section className={styles.commentForm}>
         <h3>댓글달기</h3>
         <form className={styles.commentSection}>
-          <textarea className={styles.textarea} placeholder="댓글을 입력하세요..." />
+          <textarea className={styles.textarea} placeholder="댓글을 입력해주세요..." />
           <div className={styles.submitBtnPos}>
             <button className={styles.submitBtn}>등록</button>
           </div>
@@ -57,17 +68,39 @@ export function ArticleDetailPage() {
 
       {/* 댓글 리스트 */}
       <section className={styles.commentList}>
-        {articles.Comment?.map((comment) => (
-          <li key={comment.id} className={styles.commentItem}>
-            <p className={styles.commentContent}>{comment.context}</p>
-            <div className={styles.commentMeta}>
-              <span className={styles.commentAuthor}>{comment.author}</span>
-            </div>
-          </li>
-        ))}
+        {articles.Comment && articles.Comment.length > 0 ? (
+          articles.Comment.map((comment) => (
+            <li key={comment.id} className={styles.commentItem}>
+              <div className={styles.commentContentBox}>
+                <p className={styles.commentContent}>{comment.context}</p>
+                <EllipsisVertical className={styles.commentContentIcon} width={24} height={24} />
+              </div>
+              <div className={styles.commentHeader}>
+                <img
+                  src={comment.author?.userProfile?.photoUrl || "/images/logo.png"}
+                  alt="avatar"
+                  className={styles.avatar}
+                />
+                <div className={styles.commentAuthorBox}>
+                  <span className={styles.commentAuthor}>{comment.author?.name}</span>
+                  <p className={styles.commentUpdateTime}>{comment.createdAt}</p>
+                </div>
+              </div>
+            </li>
+          ))
+        ) : (
+          <>
+            <img className={styles.noCommentImg} src="/images/article/Img_reply_empty.svg" alt="empty-img" />
+            <p className={styles.noComment}>아직 댓글이 없어요,<br />
+              지금 댓글을 달아보세요!</p>
+          </>
+        )}
       </section>
 
-      <Link className={styles.backToArticles} to="articles">목록으로 돌아가기</Link>
-    </main>
+      <Link className={styles.backToArticles} to="/articles">
+        <p className={styles.backToArticlesBtn}>목록으로 돌아가기</p>
+        <Undo2 width={24} height={24} />
+      </Link>
+    </main >
   );
 }
