e5304cd1f02086ed12aee51b3f6d51130a487216
rklpoi5678 <rklpoi5678@naver.com>
2025-11-01 17:47:44 +0900

    chore: 프로젝트 설정 및 공통 스타일 오버라이드

---
+++ config/root-metadata.js
@@ -0,0 +1,10 @@
+
+export const metadata = {
+  title: {
+    default: '판다마켓 Next프로젝트',
+    template: '%s | Next.js 프로젝트'
+  },
+  description: "판다마켓 넥스트 프로젝트 미션-8" ,
+  keywords: ['Next.js',' React', 'Tailwind CSS'],
+  creator: 'Kim youn gi'
+}
