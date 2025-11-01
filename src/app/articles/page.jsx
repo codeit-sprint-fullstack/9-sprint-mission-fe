1f4e0a1f3883c538bb462c3c4ab97ad19f3959ab
rklpoi5678 <rklpoi5678@naver.com>
2025-11-02 01:12:59 +0900

    feat: 랜딩페이지 마이그레이션

---
+++ src/app/articles/page.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function ArticlePage() {
+
+  return (
+    <main className="max-w-full w-7xl m-0 p-5">
+      {/* 베스트 게시글 영역 */}
+
+      {/* 게시글 영역 */}
+      <section className="max-w-full m-0">
+        <div className="flex justify-between items-center mb-3">
+          <p>게시글</p>
+          <Link className="flex justify-center items-center gap-2.5 h-10.5 px-3 py-5.5 rounded-lg bg-primary-100 text-gray-100 font-pretendard text-base font-semibold leading-6.5 no-underline cursor-pointer hover:bg-primary-200 active:bg-primary-300" href="/registration">
+            글쓰기
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
+import { CTASection, FeatureSection, HeroSection } from "@/components/ui/landing";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    </div>
+    <>
+      <HeroSection />
+      <FeatureSection />
+      <CTASection />
+    </>
   );
 }
- 
