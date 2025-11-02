953a94cd267d12fc86c0040dac0d8edafc47b8c8
rklpoi5678 <rklpoi5678@naver.com>
2025-11-02 21:14:50 +0900

    feat: favicon.ico 변경

--- config/root-metadata.js
+++ config/root-metadata.js
@@ -1,10 +1,9 @@
-
 export const rootMetadata = {
   title: {
     default: '판다마켓 Next프로젝트',
-    template: '%s | Next.js 프로젝트'
+    template: '%s | Next.js 프로젝트',
   },
-  description: "판다마켓 넥스트 프로젝트 미션-8" ,
-  keywords: ['Next.js',' React', 'Tailwind CSS'],
-  creator: 'Kim youn gi'
-}
+  description: '판다마켓 넥스트 프로젝트 미션-8',
+  keywords: ['Next.js', ' React', 'Tailwind CSS'],
+  creator: 'Kim youn gi',
+};
