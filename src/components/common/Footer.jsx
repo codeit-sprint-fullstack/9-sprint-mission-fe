6e94bb777d5adca4bd35f00da9bbd29069495c86
nnnn2001 <hee2ees2@gmail.com>
2025-11-04 18:09:51 +0900

    style: footer sns 아이콘 이미지 추가

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <footer className="bg-[#111827] px-20 py-20">
@@ -7,7 +9,17 @@ export default function Footer() {
           <li>Privacy Policy</li>
           <li>FAQ</li>
         </ul>
-        <div className="text-white flex px-4">images</div>
+        <div className="text-white flex px-4 items-center gap-2">
+          <Image src="/ic_facebook.png" alt="facebook" width={20} height={20} />
+          <Image src="/ic_twitter.png" alt="twitter" width={20} height={20} />
+          <Image src="/ic_youtube.png" alt="youtube" width={20} height={20} />
+          <Image
+            src="/ic_instagram.png"
+            alt="instagram"
+            width={20}
+            height={20}
+          />
+        </div>
       </div>
     </footer>
   );
