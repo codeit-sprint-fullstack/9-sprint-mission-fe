a809f23f008474caa88f6ce23decd991ce3f2a04
nnnn2001 <hee2ees2@gmail.com>
2025-11-04 16:45:58 +0900

    feat: 홈 페이지 구현

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import PageContainer from "@/components/common/PageContainer";
+import BestItem from "@/components/ui/Best/BestIem";
+import Item from "@/components/ui/Item/Item";
+import Post from "@/app/Post/page";
+import { getArticles } from "@/lib/services/ItemApi";
+
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  const articles = await getArticles();
+  return (
+    <PageContainer>
+      <BestItem items={articles} />
+      <Item items={articles} />
+    </PageContainer>
+  );
+}
