d30d0b3fedcdee2833f7d792512aef93822041ec
rklpoi5678 <rklpoi5678@naver.com>
2025-11-04 22:24:04 +0900

    fix: 이클립스 이미지 클릭반경 수정

--- src/components/ui/dropdown/dropdown-content.jsx
+++ src/components/ui/dropdown/dropdown-content.jsx
@@ -21,17 +21,19 @@ export function DropdownContent({ comment }) {
       <div className="font-pretendard text-sm text-gray-900 leading-6">
         <p>{comment.context}</p>
       </div>
-      <div className="relative">
+      <button
+        className="relative cursor-pointer"
+        onClick={handlePanel}
+      >
         <Image
-          className="flex shrink-0 cursor-pointer pt-1.25 pr-0 pb-1.5 pl-0 mr-3.25"
+          className="flex shrink-0 pt-1.25 pr-0 pb-1.5 pl-0 mr-3.25 ml-3.25"
           src={EllipsisVertical}
-          onClick={handlePanel}
           alt="vertical-dropdown-button"
           width={3}
           height={13}
         />
         {showPanel && (
-          <ul className="absolute z-2 mt-2 shrink-0 rounded-xl border border-solid border-gray-200 bg-white -left-30">
+          <ul className="absolute z-2 mt-2 shrink-0 rounded-xl border border-solid border-gray-200 bg-white -left-20">
             {contents.map((c) => (
               <li key={c.name} className="font-pretendard cursor-point mt-0.5 flex h-10.5 w-31.25 shrink-0 items-center justify-center text-gray-800 text-lg leading-6.5">
                 <button
@@ -44,7 +46,7 @@ export function DropdownContent({ comment }) {
             ))}
           </ul>
         )}
-      </div>
+      </button>
     </>
   )
 }
