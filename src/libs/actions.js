5c05e3cd34b50f59469d5f50dfb5e8a5262f5baa
rklpoi5678 <rklpoi5678@naver.com>
2025-11-04 14:12:23 +0900

    feat: 댓글 서버액션 및 간단한  suspend 효과

---
+++ src/libs/actions.js
@@ -0,0 +1,45 @@
+'use server';
+
+import { revalidateTag } from 'next/cache';
+import { z } from 'zod';
+
+import prisma from './prisma';
+
+const schema = z.object({
+  authorId: z.int(),
+  articleId: z.int(),
+  context: z.string().min(1, '댓글 내용을 입력해주세요.'),
+});
+
+export const createComment = async (formData) => {
+  console.log(formData);
+
+  const rawAuthorId = formData.get('authorId');
+  const rawArticleId = formData.get('articleId');
+
+  try {
+    const validateSchema = schema.parse({
+      authorId: parseInt(rawAuthorId),
+      articleId: parseInt(rawArticleId),
+      context: formData.get('context'),
+    });
+
+    await prisma.comment.create({
+      data: {
+        authorId: validateSchema.authorId,
+        articleId: validateSchema.articleId,
+        context: validateSchema.context,
+      },
+    });
+
+    revalidateTag('comment', 'max');
+
+    return { message: 'Added Todo Successfully' };
+  } catch (error) {
+    console.error(error);
+    if (error instanceof z.ZodError) {
+      return { message: 'Invalid input data', errors: error.errors };
+    }
+    return { message: 'Failed to create comment' };
+  }
+};
