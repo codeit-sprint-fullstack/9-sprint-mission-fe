d16a024b574498541e58c2b9b434a3779458a559
nnnn2001 <hee2ees2@gmail.com>
2025-11-04 16:49:01 +0900

    feat: 홈 페이지 내 게시글 section 추가

---
+++ src/components/ui/Item/Item.jsx
@@ -0,0 +1,39 @@
+import ItemCard from "./ItemCard";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Item({ items }) {
+  console.log("items 데이터:", items);
+  return (
+    <div>
+      <div className="max-w-6xl mx-auto px-4 flex justify-between">
+        <h2 className="font-bold text-xl">게시글</h2>
+        <Link href="/Post">
+          <div className="bg-[#3692FF] w-23 h-10 p-2 rounded-lg text-white font-bold flex justify-center items-center">
+            글쓰기
+          </div>
+        </Link>
+      </div>
+
+      <div className="max-w-6xl mx-auto px-4 flex justify-between items-center mt-3 mb-3 gap-10">
+        <div className="absolute px-4">
+          <Image src="/ic_search.png" alt="search" width={30} height={30} />
+        </div>
+        <input
+          type="text"
+          placeholder="검색할 상품을 입력해주세요"
+          className="px-15 py-2 bg-[#F3F4F6] rounded-lg w-full placeholder:text-left"
+        ></input>
+        <select className="border border-gray-300 rounded-lg pl-3 pr-8 py-1 text-sm font-medium">
+          <option value="latest">최신순</option>
+          <option value="oldest">오래된순</option>
+          <option value="popular">인기순</option>
+        </select>
+      </div>
+      <div>
+        {Array.isArray(items?.data) &&
+          items.data.map((item) => <ItemCard key={item.id} item={item} />)}
+      </div>
+    </div>
+  );
+}
