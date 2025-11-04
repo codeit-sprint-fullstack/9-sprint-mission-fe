a26174dab1d34276a7a81e60ffdca2b16d0764da
nnnn2001 <hee2ees2@gmail.com>
2025-11-04 16:47:27 +0900

    feat: Footer 구현

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#111827] px-20 py-20">
+      <div className="flex justify-between">
+        <div className="text-[#9CA3AF] flex px-4">@codeit-2024</div>
+        <ul className="text-white flex flex-row gap-6 items-center justify-center">
+          <li>Privacy Policy</li>
+          <li>FAQ</li>
+        </ul>
+        <div className="text-white flex px-4">images</div>
+      </div>
+    </footer>
+  );
+}
