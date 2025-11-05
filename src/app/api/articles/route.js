8e483bcb6e7efaac0b03a7abc3c4f26a8e415d2c
rklpoi5678 <rklpoi5678@naver.com>
2025-11-05 15:47:40 +0900

    fix: 'use cache'로 변경

--- src/app/api/articles/route.js
+++ src/app/api/articles/route.js
@@ -2,9 +2,8 @@ import { NextResponse } from 'next/server';
 
 import prisma from '@/libs/prisma';
 
-export const dynamic = 'force-static';
-
 export const GET = async (request) => {
+  'use cache';
   try {
     const { searchParams } = new URL(request.url);
 
