2bfdc6d49017c74d85f1fc79db3b113f53aacd2c
nnnn2001 <hee2ees2@gmail.com>
2025-11-09 21:08:28 +0900

    style: BestItemCard 카드 ui 흔들림 수정

--- src/components/ui/Best/BestItemCard.jsx
+++ src/components/ui/Best/BestItemCard.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function BestItemCard({ item }) {
   return (
-    <article>
-      <div className="bg-[#F9FAFB] rounded-lg mb-10 mt-7">
+    <article className="w-[360px]">
+      <div className="bg-[#F9FAFB] rounded-lg mb-10 mt-7 flex flex-col justify-between">
         <Image
           src="/img_badge.png"
           alt="badge"
@@ -11,24 +11,25 @@ export default function BestItemCard({ item }) {
           height={100}
           className="px-4"
         />
-        <div className="flex items-center justify-center gap-1 p-3">
-          <div className="p-1 flex flex-col gap-5">
-            <div className="flex justify-center items-center gap-10">
-              <p className="font-bold text-xl">{item.content}</p>
-              <div className="border border-[#E5E7EB] rounded-lg p-2 bg-white">
-                <Image src="/img_71.png" alt="img" width={70} height={50} />
-              </div>
+        <div className="flex flex-col gap-5 p-4">
+          <div className="p-1 flex gap-5">
+            <p className="font-bold text-lg leading-tight line-clamp-2 min-h-12 w-[300px]">
+              {item.title}
+            </p>
+            <div className="border border-[#E5E7EB] rounded-lg p-2 bg-white shrink-0">
+              <Image src="/img_71.png" alt="img" width={70} height={50} />
             </div>
-            <div className="flex justify-between items-center">
-              <div className="flex gap-2 items-center justify-center">
-                <p className="text-[#4B5563] text-sm">{item.id}</p>
-                <Image src="/ic_heart.png" alt="heart" width={15} height={15} />
-                <p className="text-sm text-[#4B5563] ">9999+</p>
-              </div>
-              <p className="text-[#9CA3AF] text-sm">
-                {new Date(item.createdAt).toLocaleDateString("ko-KR")}
-              </p>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <div className="flex gap-2 items-center">
+              <p className="text-[#4B5563] text-sm">{item.id}</p>
+              <Image src="/ic_heart.png" alt="heart" width={15} height={15} />
+              <p className="text-sm text-[#4B5563] ">9999+</p>
             </div>
+            <p className="text-[#9CA3AF] text-sm">
+              {new Date(item.createdAt).toLocaleDateString("ko-KR")}
+            </p>
           </div>
         </div>
       </div>
