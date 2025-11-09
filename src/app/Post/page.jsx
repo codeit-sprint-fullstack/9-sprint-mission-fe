37a6c4be7a81eafa086e0d0671b7c48aa8a013c6
nnnn2001 <hee2ees2@gmail.com>
2025-11-09 20:58:31 +0900

    feat: 게시글 작성 기능 추가

--- src/app/Post/page.jsx
+++ src/app/Post/page.jsx
@@ -1,37 +1,48 @@
 "use client";
-import { useState } from "react";
+import { addArticle } from "@/lib/services/ItemApi";
+import { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Post() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const formRef = useRef(null);
+  const router = useRouter();
 
   const isFormValid = title.trim() !== "" && content.trim() !== "";
 
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch("/api/Post", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ title, content }),
-      });
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    const content = formData.get("content");
 
-      if (!response.ok) throw new Error("게시물 등록 실패");
+    if (!title || !content) return;
 
-      alert("게시물 등록 성공");
+    const result = await addArticle({
+      title,
+      content: content.toString(),
+    });
+
+    if (result.success) {
+      formRef.current?.reset();
       setTitle("");
       setContent("");
-    } catch (error) {
-      alert("게시물을 등록할 수 없습니다");
+      router.push("/");
+      router.refresh();
+    } else {
+      alert(result.error || "게시글 작성 실패");
     }
-  };
+  }
 
   return (
     <div>
       <div className="max-w-6xl mx-auto px-4 mt-5 mb-5 flex justify-between">
         <h2 className="font-bold text-xl">게시글 쓰기</h2>
         <button
+          type="submit"
+          form="postForm"
           disabled={!isFormValid}
-          onClick={handleSubmit}
           className={`bg-[#9CA3AF] w-16 h-10 p-2 rounded-lg text-white font-bold flex justify-center items-center
           ${isFormValid ? "bg-[#3692FF] cursor-pointer" : "bg-gray-300"}`}
         >
@@ -39,26 +50,31 @@ export default function Post() {
         </button>
       </div>
 
-      <div className="max-w-6xl mx-auto px-4 flex flex-col ">
+      <form
+        id="postForm"
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className="max-w-6xl mx-auto px-4 flex flex-col "
+      >
         <h2 className="mb-4 font-bold">*제목</h2>
-        <input
+        <textarea
+          name="title"
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder="제목을 입력해주세요"
           className="px-4 py-5 bg-[#F3F4F6] rounded-lg placeholder:text-left w-full"
         />
-      </div>
 
-      <div className="max-w-6xl mx-auto px-4 flex flex-col ">
         <h2 className="mt-5 mb-5 font-bold">*내용</h2>
         <textarea
+          name="content"
           placeholder="내용을 입력해주세요"
           value={content}
           onChange={(e) => setContent(e.target.value)}
           className="p-4 h-64 bg-[#F3F4F6] rounded-lg text-start align-top w-full mb-80"
         />
-      </div>
+      </form>
     </div>
   );
 }
