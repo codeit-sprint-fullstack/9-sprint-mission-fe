2bd4915483e7395b4ab33747adb8ae5acd305af8
rklpoi5678 <rklpoi5678@naver.com>
2025-11-11 16:20:55 +0900

    Feat: [PRJ-9] 내려줄 프로바이더 생성

---
+++ src/providers/auth-provider.jsx
@@ -0,0 +1,65 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+import { authService } from "@/services/auth-service";
+import { userService } from "@/services/user-service";
+
+const AuthContext = createContext({
+  login: () => { },
+  logout: () => { },
+  user: null,
+  updateUser: () => { },
+  register: () => { },
+});
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider")
+  }
+  return context;
+};
+
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  const getUser = async () => {
+    try {
+      const user = await userService.getMe();
+      setUser(user)
+    } catch (error) {
+      console.error("사용자 정보를 가져오는데 실패했습니다:.", error)
+      setUser(null)
+    } finally {
+      setIsInitialized(true)
+    }
+  };
+
+  const register = async (name, email, password) => {
+    await authService.register(name, email, password)
+  }
+
+  const login = async (email, password) => {
+    await authService.login(email, password);
+    await getUser();
+  }
+
+  const updateUser = async (user) => {
+    const updatedUser = await userService.updateMe(user);
+    setUser(updatedUser);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      getUser();
+    }, 0)
+  }, [])
+
+  return (
+    <AuthProvider.Provider value={{ user, login, updateUser, register, isInitialized }}>
+      {children}
+    </AuthProvider.Provider>
+  )
+}
+
+
