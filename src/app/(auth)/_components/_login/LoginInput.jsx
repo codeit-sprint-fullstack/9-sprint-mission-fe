127f5e92aaffb4ce87b37e4cb7baddfe683e48d4
p-changki <dnsxj12345aa@gmail.com>
2025-11-12 01:17:13 +0900

    ✨ feat : 비밀번호 검증 로직 완성

--- src/app/(auth)/_components/_login/LoginInput.jsx
+++ src/app/(auth)/_components/_login/LoginInput.jsx
@@ -8,6 +8,8 @@ export default function LoginInput() {
   const [showEye, setShowEye] = useState(false);
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -15,9 +17,24 @@ export default function LoginInput() {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (e.target.value && !emailRegex.test(e.target.value)) {
-      setEmailError("잘못된 이메일입니다.");
+      setEmailError("잘못된 이메일입니다!");
     } else {
       setEmailError("");
+    }
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+
+    const passwordRegEx =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+    if (e.target.value && !passwordRegEx.test(e.target.value)) {
+      setPasswordError(
+        "비밀번호는 8자 이상, 영문·숫자·특수문자를 포함해야 합니다!"
+      );
+    } else {
+      setPasswordError("");
     }
   };
 
@@ -48,11 +65,19 @@ export default function LoginInput() {
         <div className="relative ">
           <input
             type={showEye ? "text" : "password"}
+            onChange={handlePassword}
+            value={password}
             placeholder="비밀번호를 입력해주세요"
-            className="border-0 bg-gray-100  w-[640px] items-center rounded-xl h-14 px-6 py-4 "
+            className={clsx(
+              "border-0 bg-gray-100  w-[640px] items-center rounded-xl h-14 px-6 py-4 ",
+              { "outline-1 outline-red-400": passwordError }
+            )}
           />
+          {passwordError && (
+            <p className="text-red-500 text-sm mt-2">{passwordError}</p>
+          )}
           <button
-            type="button"
+            type="submit"
             onClick={() => setShowEye((prev) => !prev)}
             className="absolute right-4 top-1/2 -translate-y-1/2"
           >
