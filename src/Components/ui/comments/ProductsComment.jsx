a9d371f2564f89248e338593b671eb892593dbfa
p-changki <dnsxj12345aa@gmail.com>
2025-11-19 01:02:30 +0900

    feat : comments 엔터키 추가!

--- src/Components/ui/comments/ProductsComment.jsx
+++ src/Components/ui/comments/ProductsComment.jsx
@@ -16,6 +16,13 @@ export default function ProductsComment() {
     setContent(e.target.value);
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); // ✔ 줄바꿈 방지
+      handleSubmit();
+    }
+  };
+
   const handleSubmit = () => {
     const cleanContent = content.trim();
 
@@ -37,6 +44,7 @@ export default function ProductsComment() {
           type="text"
           value={content}
           onChange={handleInput}
+          onKeyDown={handleKeyDown}
           placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법
         정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은
         게시자에게 있습니다."
