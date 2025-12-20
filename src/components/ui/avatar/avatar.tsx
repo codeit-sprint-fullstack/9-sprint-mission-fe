c57dbb29d66fc8e65743ac90d54049b0702cb28e
rklpoi5678 <rklpoi5678@naver.com>
2025-12-21 05:03:17 +0900

    Refactor: 점진적 마이그레이션 (아바타)

--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -1,9 +1,11 @@
-import { cva } from "class-variance-authority"
-import Image from "next/image"
+import { cva, type VariantProps } from "class-variance-authority"
+import Image, { type StaticImageData } from "next/image"
+import { type HTMLAttributes } from "react"
 
 import DefaultAvatar from "@/assets/ic_default_avatar.svg"
 import { cn } from "@/libs/cn"
 
+/** 공용 컴포넌트는 HTML요소의 속성을 확장하는것을 권장 (재사용성 이 중요하기에)*/
 const avatarVariants = cva(
   "relative bg-gray-300 rounded-full shrink-0 overflow-hidden",
   {
@@ -21,7 +23,16 @@ const avatarVariants = cva(
   }
 );
 
-export function Avatar({ className = "", size = "medium", src, alt, ...props }) {
+/**
+ *  avatar는 보통 div에 연결됨
+ *  경로는 문자열이거나 import된 이미지 객체 일수도 있음 
+ */
+interface AvatarProps extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof avatarVariants> {
+  src?: string | StaticImageData | null;
+  alt?: string;
+}
+
+export function Avatar({ className = "", size = "medium", src, alt = 'author-avatar', ...props }: AvatarProps) {
   const mergedClass = cn(
     avatarVariants({ size }),
     className
