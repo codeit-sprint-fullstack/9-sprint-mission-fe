1815e2628701f6bdd6b5c64330b0e87dc964fa50
rklpoi5678 <rklpoi5678@naver.com>
2025-12-21 17:01:24 +0900

    Refactor: 점진적 마이그레이션(Search컴포넌트)

--- src/components/ui/search/search.tsx
+++ src/components/ui/search/search.tsx
@@ -1,23 +1,35 @@
 "use client"
 import Image from 'next/image'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import type { ChangeEvent } from 'react'
 
 import SearchIcon from '@/assets/icons/ic_searchs.svg'
+import { useDebouncedCallback } from '@/hooks/useDebouncedCallback'
 
-export function Search({ placeholder }) {
+interface SearchProps {
+  placeholder?: string
+}
+
+export function Search({ placeholder }: SearchProps) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  // TODOS: 디바운싱
-  function handleSearch(term) {
-    const params = new URLSearchParams(searchParams)
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams.toString());
     if (term) {
-      params.set('keyword', term)
+      params.set('keyword', term);
     } else {
       params.delete('keyword');
     }
+    // 검색 시 페이지 초기화
+    params.set("page", "1");
+
     replace(`${pathname}?${params.toString()}`);
+  }, 300)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    handleSearch(e.target.value);
   }
 
   return (
@@ -27,7 +39,7 @@ export function Search({ placeholder }) {
         htmlFor="search">
         <Image
           src={SearchIcon}
-          alt="search-icon"
+          alt="검색 아이콘"
           unoptimized
         />
       </label>
@@ -35,9 +47,7 @@ export function Search({ placeholder }) {
         className='py-2.25 pr-5 pb-2.25 pl-10 w-full bg-gray-100 text-gray-500 font-pretendard leading-6.5 text-lg rounded-xl border-none focus:text-gray-950'
         id='search'
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value)
-        }}
+        onChange={handleChange}
         defaultValue={searchParams.get('keyword')?.toString()}
       />
     </div>
