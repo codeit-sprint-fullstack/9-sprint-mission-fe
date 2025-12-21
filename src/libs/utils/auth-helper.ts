18c071bf3210a2f50dcf1db4b2d074e4479ae1f3
rklpoi5678 <rklpoi5678@naver.com>
2025-12-21 17:48:08 +0900

    Refactor: 점진적 마이그레이션(토큰 검증)

--- src/libs/utils/auth-helper.ts
+++ src/libs/utils/auth-helper.ts
@@ -1,9 +1,13 @@
-import jwt from 'jsonwebtoken';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
 import { cookies } from 'next/headers';
 
 import { apiResponse } from './api-helper';
 
 const JWT_SECRET = process.env.JWT_SECRET;
+
+interface CustomJwtPayload extends JwtPayload {
+  userID: string;
+}
 
 export const getUserIdFromToken = async () => {
   const cookieStore = await cookies();
@@ -13,8 +17,15 @@ export const getUserIdFromToken = async () => {
     return apiResponse(false, '인증이 필요합니다.', null, 401);
   }
 
+  if (!JWT_SECRET) {
+    throw new Error('JWT_SECRET이 환경 변수에 설정되지  않았습니다.');
+  }
+
   try {
-    const decodedToken = jwt.verify(accessTokenCookie.value, JWT_SECRET);
+    const decodedToken = jwt.verify(
+      accessTokenCookie.value,
+      JWT_SECRET,
+    ) as CustomJwtPayload;
     return decodedToken.userId;
   } catch (error) {
     return apiResponse(false, '유효하지 않거나 만료된 토큰', null, 401);
