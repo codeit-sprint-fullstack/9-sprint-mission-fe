e4f65e1572480452c59e4c96a981297375080fbb
rklpoi5678 <rklpoi5678@naver.com>
2025-12-22 01:28:58 +0900

    Fix: articleSchema 업데이트

--- src/libs/schemas/article.schema.ts
+++ src/libs/schemas/article.schema.ts
@@ -1,32 +1,34 @@
 import { z } from 'zod';
 
-const articleFormSchema = z.object({
-  title: z
-    .string()
-    .min(1, '제목을 입력해주세요.')
-    .max(30, '제목은 30자 이내로 입력해주세요.'),
-  content: z
-    .string()
-    .min(10, '내용은 10자 이상 입력해주세요.')
-    .max(100, '내용은 100자 이내로 입력해주세요.'),
+const titleRule = z
+  .string()
+  .min(1, '제목을 입력해주세요.')
+  .max(30, '제목은 30자 이내로 입력해주세요.')
+  .trim();
+
+const contentRule = (min: number, max: number) => {
+  z.string()
+    .min(min, `내용은 ${min}자 이상 입력해주세요.`)
+    .max(100, `내용은 ${max}자 이내로 입력해주세요.`)
+    .trim();
+};
+
+export const articleFormSchema = z.object({
+  title: titleRule,
+  content: contentRule(10, 100),
 });
 
-const updateCommentSchema = z.object({
-  context: z.string().min(1, '수정할 댓글 내용을 입력해주세요.'),
-});
-
-const articleCommentSchema = z.object({
+export const articleCommentSchema = z.object({
   context: z.string().min(1, '댓글 내용을 입력해주세요'),
 });
 
-const updateArticleCommentSchema = z.object({
+export const updateArticleCommentSchema = z.object({
   commentId: z.number({ message: '유효한 댓글 ID가 아닙니다.' }),
   context: z.string().min(1, '수정할 댓글 내용을 입력해주세요.'),
 });
 
-export {
-  articleCommentSchema,
-  articleFormSchema,
-  updateArticleCommentSchema,
-  updateCommentSchema,
-};
+export type ArticleFormValues = z.infer<typeof articleFormSchema>;
+export type ArticleCommentValues = z.infer<typeof articleCommentSchema>;
+export type UpdateCommentFormValues = z.infer<
+  typeof updateArticleCommentSchema
+>;
