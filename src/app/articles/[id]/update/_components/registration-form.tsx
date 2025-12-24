3625baa580d27d623b6fec75d290796838d2122f
rklpoi5678 <rklpoi5678@naver.com>
2025-12-24 18:03:34 +0900

    Fix: 수정시 기존 데이터가 안불러지던 오류 수정

--- src/app/articles/[id]/update/_components/registration-form.tsx
+++ src/app/articles/[id]/update/_components/registration-form.tsx
@@ -1,16 +1,19 @@
-"use client"
-import { zodResolver } from '@hookform/resolvers/zod'
+'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
-import { use, useEffect, useState } from "react";
+import { use, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { Button } from '@/components/ui/button';
-import { Modal } from "@/components/ui/dialog";
-import { articleFormSchema, type ArticleFormValues } from '@/libs/schemas/article.schema';
+import { Modal } from '@/components/ui/dialog';
+import {
+  articleFormSchema,
+  type ArticleFormValues,
+} from '@/libs/schemas/article.schema';
 import { articleService } from '@/services/article-service';
 
 interface ArticleRegistrationProps {
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
 }
 export function ArticleRegistration({ params }: ArticleRegistrationProps) {
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -19,7 +22,7 @@ export function ArticleRegistration({ params }: ArticleRegistrationProps) {
   // const params = useParams<{ id: string }>()
   // const id = params.id;
   // React19 use hook 사용 (Promise -> resolve 까지 대기 후  결과값 반환)
-  const { id } = use(params)
+  const { id } = use(params);
   const router = useRouter();
 
   const {
@@ -31,39 +34,40 @@ export function ArticleRegistration({ params }: ArticleRegistrationProps) {
     resolver: zodResolver(articleFormSchema),
     mode: 'onChange', // 실시간 유효성 검사
     defaultValues: {
-      title: "",
-      content: "",
-    }
-  })
+      title: '',
+      content: '',
+    },
+  });
 
   // 기존 데이터 불러오기 (수정시)
   useEffect(() => {
     if (id) {
       const fetchArticle = async () => {
         try {
-          const data = await articleService.getArticlesById(id);
+          const response = await articleService.getArticlesById(id);
+          const { title, content } = response.data;
           reset({
-            title: data.title,
-            content: data.content
-          })
+            title,
+            content,
+          });
         } catch (error) {
-          console.error("데이터 로드 실패:", error)
+          console.error('데이터 로드 실패:', error);
         }
-      }
+      };
       fetchArticle();
     }
-  }, [id, reset])
+  }, [id, reset]);
 
   const onSubmit = async (data: ArticleFormValues) => {
     if (isSubmitting) return;
     setIsSubmitting(true);
     try {
-      await articleService.updateArticle(id, data)
+      await articleService.updateArticle(id, data);
 
-      router.push(`/articles/${id}`)
-      router.refresh()
+      router.push(`/articles/${id}`);
+      router.refresh();
     } catch (error) {
-      console.error("등록중 오류 발생:", error);
+      console.error('등록중 오류 발생:', error);
       setShowModal(true);
     } finally {
       setIsSubmitting(false);
@@ -75,22 +79,19 @@ export function ArticleRegistration({ params }: ArticleRegistrationProps) {
       <form
         onSubmit={handleSubmit(onSubmit)}
         autoComplete="off"
-        className='px-6 w-full'
+        className="w-full px-6"
       >
-        <div className="flex w-full justify-between mb-9">
-          <h2 className='font-pretendard text-2xl font-bold leading-9'>
+        <div className="mb-9 flex w-full justify-between">
+          <h2 className="font-pretendard text-2xl leading-9 font-bold">
             게시물 수정하기
           </h2>
-          <Button
-            type="submit"
-            disabled={!isValid || isSubmitting}
-          >
+          <Button type="submit" disabled={!isValid || isSubmitting}>
             {isSubmitting ? '수정 중...' : '등록'}
           </Button>
         </div>
-        <div className="flex flex-col gap-4 w-full mb-8">
+        <div className="mb-8 flex w-full flex-col gap-4">
           <label
-            className='text-gray-800 font-pretendard text-lg font-bold leading-6.5'
+            className="font-pretendard text-lg leading-6.5 font-bold text-gray-800"
             htmlFor="title"
           >
             제목
@@ -98,32 +99,32 @@ export function ArticleRegistration({ params }: ArticleRegistrationProps) {
           <input
             id="title"
             type="text"
-            className='w-full h-14 py-4 px-6 rounded-xl bg-gray-100'
+            className="h-14 w-full rounded-xl bg-gray-100 px-6 py-4"
             placeholder="제목을 입력해주세요"
             aria-label="제목을 입력해주세요"
             {...register('title')}
           />
           {errors.title && (
-            <span className='text-error-red'>{errors.title.message}</span>
+            <span className="text-error-red">{errors.title.message}</span>
           )}
         </div>
 
-        <div className="flex flex-col gap-4 w-full mb-8">
+        <div className="mb-8 flex w-full flex-col gap-4">
           <label
-            className='text-gray-800 font-pretendard text-lg font-bold leading-6.5'
+            className="font-pretendard text-lg leading-6.5 font-bold text-gray-800"
             htmlFor="content"
           >
             내용
           </label>
           <textarea
             id="content"
-            className='max-w-full w-full h-70.5 py-4 px-6 resize-none border-0 rounded-xl bg-gray-100'
+            className="h-70.5 w-full max-w-full resize-none rounded-xl border-0 bg-gray-100 px-6 py-4"
             placeholder="내용을 입력해주세요"
             aria-label="내용을 입력해주세요"
             {...register('content')}
           />
           {errors.content && (
-            <span className='text-error-red'>{errors.content.message}</span>
+            <span className="text-error-red">{errors.content.message}</span>
           )}
         </div>
       </form>
@@ -132,11 +133,10 @@ export function ArticleRegistration({ params }: ArticleRegistrationProps) {
         <Modal
           close={() => setShowModal(false)}
           msg={
-            "등록중 예기치 못한 오류가 발생했습니다.\n 잠시후 다시시도해 주십시오 \n 문의(meta-os@zohomail.com)"
+            '등록중 예기치 못한 오류가 발생했습니다.\n 잠시후 다시시도해 주십시오 \n 문의(meta-os@zohomail.com)'
           }
         />
       )}
     </>
   );
 }
-
