a89ef7c7cf017e0e150a7f1a364eed6bb84e733b
rklpoi5678 <rklpoi5678@naver.com>
2025-12-24 18:04:51 +0900

    Fix: 프리티어

--- src/providers/route-guard.tsx
+++ src/providers/route-guard.tsx
@@ -1,50 +1,44 @@
-"use client"
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+'use client';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
-import { useAuth } from "./auth-provider";
+import { useAuth } from './auth-provider';
 
 interface RouteGuardProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const PROTECTED_PATHS = [
-  '/articles',
-  '/items',
-] as const
+const PROTECTED_PATHS = ['/articles', '/items'] as const;
 
-const PUBLIC_PATHS = [
-  '/',
-  '/login',
-  '/signup'
-] as const
+const PUBLIC_PATHS = ['/', '/login', '/signup'] as const;
 
 export default function RouteGuard({ children }: RouteGuardProps) {
-  const { user, isInitialized } = useAuth()
+  const { user, isInitialized } = useAuth();
   const router = useRouter();
   const pathname = usePathname();
 
   useEffect(() => {
     if (!isInitialized) return;
-    const currentPath = pathname?.split("?")[0] ?? "";
+    const currentPath = pathname?.split('?')[0] ?? '';
 
     const isProtectedRoute = PROTECTED_PATHS.some(
-      (route) => currentPath === route || currentPath.startsWith(`${route}/`)
+      (route) => currentPath === route || currentPath.startsWith(`${route}/`),
     );
 
     const isPublicRoute = PUBLIC_PATHS.some(
-      (route) => currentPath === route || (currentPath.startsWith(route + "/") && route !== "/")
-    )
+      (route) =>
+        currentPath === route ||
+        (currentPath.startsWith(route + '/') && route !== '/'),
+    );
 
     if (isProtectedRoute && !user) {
-      router.push("/login")
+      router.push('/login');
     }
 
-    if (isPublicRoute && user && currentPath !== "/") {
-      router.push("/articles")
+    if (isPublicRoute && user && currentPath !== '/') {
+      router.push('/articles');
     }
-
-  }, [user, pathname, router, isInitialized])
+  }, [user, pathname, router, isInitialized]);
 
   if (!isInitialized) return null;
 
