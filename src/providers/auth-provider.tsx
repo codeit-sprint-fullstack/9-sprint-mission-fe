e8dd8340155d26f00a0e51a75fd9291af563a92f
rklpoi5678 <rklpoi5678@naver.com>
2025-12-24 17:47:49 +0900

    Fix: auth프로바이더에서 무한루프현상 수정 및 패치클라이언트에서 수정된 타입사용

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -1,81 +1,97 @@
-"use client"
-import { createContext, useCallback, useContext, useEffect, useState } from "react";
+'use client';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
-import type { loginFormSchema, signupFormSchema } from "@/libs/schemas/auth.schema";
-import { authService } from "@/services/auth-service";
-import { userService } from "@/services/user-service";
-import type { User } from "@/types/auth";
+import type {
+  loginFormSchema,
+  signupFormSchema,
+} from '@/libs/schemas/auth.schema';
+import { authService } from '@/services/auth-service';
+import { userService } from '@/services/user-service';
+import type { User } from '@/types/auth';
 
 interface AuthContextType {
   user: User | null;
   isInitialized: boolean;
-  login: (data: loginFormSchema) => Promise<void>
-  signUp: (data: signupFormSchema) => Promise<void>
+  login: (data: loginFormSchema) => Promise<void>;
+  signUp: (data: signupFormSchema) => Promise<void>;
   logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const useAuth = () => {
-  const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
   if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider")
+    throw new Error('useAuth must be used within an AuthProvider');
   }
   return context;
 };
 
-export default function AuthProvider({ children }: { children: React.ReactNode }) {
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [user, setUser] = useState<User | null>(null);
   const [isInitialized, setIsInitialized] = useState<boolean>(false);
 
   const getUser = useCallback(async () => {
     try {
       const response = await userService.getMe();
-      if (response.ok) {
-        setUser(user)
+      if (response.success) {
+        setUser(response.data);
+      } else {
+        setUser(null);
       }
+      console.log(user);
     } catch (error) {
-      console.error('사용자 정보 가져오기 실패', error)
-      setUser(null)
+      console.error('사용자 정보 가져오기 실패', error);
+      setUser(null);
     } finally {
-      setIsInitialized(true)
+      setIsInitialized(true);
     }
-  }, [user]);
+  }, []);
 
   useEffect(() => {
     getUser();
-  }, [getUser])
+  }, [getUser]);
 
   const login = async (data: loginFormSchema) => {
     try {
       await authService.login(data);
       await getUser();
     } catch (error) {
-      console.error('로그인 실패:', error)
-      throw error
+      console.error('로그인 실패:', error);
+      throw error;
     }
   };
 
-  //TODO: logout 기능 추후 
-  const signUp = async (
-    data: signupFormSchema
-  ) => {
+  //TODO: logout 기능 추후
+  const signUp = async (data: signupFormSchema) => {
     try {
-      await authService.register(data)
+      await authService.register(data);
       await login({ email: data.email, password: data.password });
     } catch (error) {
-      console.error('회원가입 실패:', error)
-      throw error
+      console.error('회원가입 실패:', error);
+      throw error;
     }
-  }
+  };
 
   const logout = () => {
     setUser(null);
-  }
+  };
 
   return (
-    <AuthContext.Provider value={{ user, login, signUp, isInitialized, logout }}>
+    <AuthContext.Provider
+      value={{ user, login, signUp, isInitialized, logout }}
+    >
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
