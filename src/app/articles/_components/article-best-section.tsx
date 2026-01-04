6e55e0f9244532a531a2883cb7ead777b4296508
rklpoi5678 <rklpoi5678@naver.com>
2026-01-04 19:18:40 +0900

    Fix: User타입에 맞는 nickname속성으로 변경

--- src/app/articles/_components/article-best-section.tsx
+++ src/app/articles/_components/article-best-section.tsx
@@ -1,46 +1,46 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-import Badge from '@/assets/icons/ic_medal.svg'
-import DefaultImg from '@/assets/logo.svg'
-import { cn } from "@/libs/cn";
-import { formatDate, truncateText } from "@/libs/utils/format";
-import type { Article } from "@/types/article";
-import { paths } from "#/config/paths";
+import Badge from '@/assets/icons/ic_medal.svg';
+import DefaultImg from '@/assets/logo.svg';
+import { cn } from '@/libs/cn';
+import { formatDate, truncateText } from '@/libs/utils/format';
+import type { Article } from '@/types/article';
+import { paths } from '#/config/paths';
 
 interface ArticleBestSectionProps {
-  articles: Article[]
+  articles: Article[];
 }
 
 export function ArticleBestSection({ articles }: ArticleBestSectionProps) {
   return (
     <section className="mb-12">
-      <h2 className="font-pretendard text-xl font-bold mb-6 text-gray-900">베스트 게시글</h2>
-      <div className="flex max-w-full my-0 mx-auto gap-6">
+      <h2 className="font-pretendard mb-6 text-xl font-bold text-gray-900">
+        베스트 게시글
+      </h2>
+      <div className="mx-auto my-0 flex max-w-full gap-6">
         {articles.map((article, index) => {
           const imageSource = article.images?.[0] || DefaultImg;
           return (
             <Link
               href={paths.app.articleDetail.getHref(article.id)}
               key={article.id}
-              // 1개 일때 모두숨기고 하나씩 보여주기 
-              className={cn("relative flex flex-col justify-center items-center flex-1 w-96 h-48.5 py-0 px-6 rounded-lg overflow-hidden bg-gray-50 text-gray-900 cursor-pointer duration-200 no-underline hover: translate-y-[-4px]",
-                index >= 1 && "hidden",
-                index === 1 && "md:flex",
-                index === 2 && "xl:flex"
+              // 1개 일때 모두숨기고 하나씩 보여주기
+              className={cn(
+                'hover: relative flex h-48.5 w-96 flex-1 translate-y-[-4px] cursor-pointer flex-col items-center justify-center overflow-hidden rounded-lg bg-gray-50 px-6 py-0 text-gray-900 no-underline duration-200',
+                index >= 1 && 'hidden',
+                index === 1 && 'md:flex',
+                index === 2 && 'xl:flex',
               )}
             >
-              <div className="absolute top-0 left-[6%] flex items-center justify-center gap-1 w-25.5 py-0.5 px-6 bg-primary-100 rounded-tr-none rounded-tl-none rounded-b-2xl rounded-l-2xl">
-                <Image
-                  src={Badge}
-                  alt="Best-Badge"
-                  width={16}
-                  height={16}
-                />
-                <p className="font-pretendard font-semibold  leading-6.5 text-white">Best</p>
+              <div className="bg-primary-100 absolute top-0 left-[6%] flex w-25.5 items-center justify-center gap-1 rounded-l-2xl rounded-tl-none rounded-tr-none rounded-b-2xl px-6 py-0.5">
+                <Image src={Badge} alt="Best-Badge" width={16} height={16} />
+                <p className="font-pretendard leading-6.5 font-semibold text-white">
+                  Best
+                </p>
               </div>
-              <div className="flex flex-row-reverse w-full justify-between gap-2 mt-4">
-                <div className="flex w-18 h-18 px-3.5 py-3 bg-white border border-solid border-gray-200">
+              <div className="mt-4 flex w-full flex-row-reverse justify-between gap-2">
+                <div className="flex h-18 w-18 border border-solid border-gray-200 bg-white px-3.5 py-3">
                   <Image
                     src={imageSource}
                     alt="썸네일"
@@ -48,18 +48,21 @@ export function ArticleBestSection({ articles }: ArticleBestSectionProps) {
                     height={44.571}
                   />
                 </div>
-                <p className="font-pretendard text-xl font-semibold text-gray-800 mb-1.5 ">
+                <p className="font-pretendard mb-1.5 text-xl font-semibold text-gray-800">
                   {truncateText(article.title, 50)}
                 </p>
               </div>
-              <div className="flex w-full justify-between p-2.5 items-center">
+              <div className="flex w-full items-center justify-between p-2.5">
                 <div className="flex text-xs text-gray-500">
-                  {truncateText(article.author.name, 10)} · 조회수 {article.view}
+                  {truncateText(article.author.nickname, 10)} · 조회수{' '}
+                  {article.view}
                 </div>
-                <div className="font-pretendard text-sm font-normal  leading-6 text-gray-400">{formatDate(article.createdAt)}</div>
+                <div className="font-pretendard text-sm leading-6 font-normal text-gray-400">
+                  {formatDate(article.createdAt)}
+                </div>
               </div>
             </Link>
-          )
+          );
         })}
       </div>
     </section>
